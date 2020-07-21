b0e1f3600848ecda005397650a98877e6c1ac789
MOTORIST <webartmotor@gmail.com>
2020-07-21 17:01:47 +0400

    add _allSettled and _finally

--- promise-polyfills.js
+++ promise-polyfills.js
@@ -40,3 +40,54 @@ Promise._anyV2FromAll = function (iterable) {
     (result) => Promise.resolve(result)
   );
 };
+
+Promise._allSettled = function (iterable) {
+  const results = [];
+
+  const isIterable = (obj) =>
+    obj !== undefined &&
+    obj !== null &&
+    typeof obj[Symbol.iterator] === "function";
+
+  if (!isIterable(iterable)) {
+    throw new Error(`${typeof iterable} ${iterable} is not iterable`);
+  }
+
+  if (iterable.length === 0) {
+    return Promise.resolve([]);
+  }
+
+  return new Promise((resolve) => {
+    iterable.forEach((v) => {
+      Promise.resolve(v)
+        .then((res) => {
+          const fulfilledObj = { status: "fulfilled", value: res };
+
+          if (results.push(fulfilledObj) === iterable.length) {
+            resolve(results);
+          }
+        })
+        .catch((err) => {
+          const rejectedObj = { status: "rejected", reason: err };
+
+          if (results.push(rejectedObj) === iterable.length) {
+            resolve(results);
+          }
+        });
+    });
+  });
+};
+
+Promise.prototype._finally = function (callback) {
+  const callbackIsFunction = typeof callback === "function";
+
+  return new Promise((resolve, reject) => {
+    this.then((res) => {
+      callbackIsFunction && callback();
+      resolve(res);
+    }).catch((err) => {
+      callbackIsFunction && callback();
+      reject(err);
+    });
+  });
+};
--- promise-polyfills.test.js
+++ promise-polyfills.test.js
@@ -81,3 +81,64 @@ describe("Promise _anyV2FromAll", () => {
     return expect(result).resolves.toBe(1);
   });
 });
+
+describe("Promise _allSettled", () => {
+  it("should return all results", () => {
+    const expected = [
+      { status: "fulfilled", value: 1 },
+      { status: "rejected", reason: new Error(2) },
+      { status: "fulfilled", value: 3 },
+    ];
+
+    const iterable = expected.map((v) =>
+      v.status === "fulfilled"
+        ? Promise.resolve(v.value)
+        : Promise.reject(v.reason)
+    );
+
+    const result = Promise._allSettled(iterable);
+
+    return expect(result).resolves.toEqual(expect.arrayContaining(expected));
+  });
+
+  it("should throw error, if argument is not iterable", () => {
+    const result = () => Promise._allSettled({ id: 1 });
+
+    expect(result).toThrowError(/iterable/);
+  });
+
+  it("should return empty array, if argument empty array", () => {
+    const result = Promise._allSettled([]);
+
+    return expect(result).resolves.toEqual([]);
+  });
+
+  it("should work with primitive values", () => {
+    const values = [1, "2", false];
+    const expected = values.map((value) => ({ status: "fulfilled", value }));
+
+    const result = Promise._allSettled(values);
+
+    return expect(result).resolves.toEqual(expected);
+  });
+});
+
+describe("Promise _finally", () => {
+  it("should call, if promise resolved", async () => {
+    const callback = jest.fn();
+
+    await Promise.resolve(1)._finally(callback);
+
+    expect(callback.mock.calls.length).toBe(1);
+  });
+
+  it("should call, if promise rejected", async () => {
+    const callback = jest.fn();
+
+    try {
+      await Promise.reject(new Error())._finally(callback);
+    } catch (err) {}
+
+    expect(callback.mock.calls.length).toBe(1);
+  });
+});
